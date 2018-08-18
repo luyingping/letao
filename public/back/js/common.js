2548dfc807e4a52f5a0538433ad04eecf8acb4d6
lyp <347989770@qq.com>
2018-08-18 23:53:19 +0800

    首页公共部分功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,4 +1,4 @@
-
+//进度条事件
 $(document).ajaxStart(function(){
   NProgress.start();
 });
@@ -8,4 +8,55 @@ $(document).ajaxStop(function () {
   setTimeout(function(){
     NProgress.done();
   },500)
-})
+});
+//登录拦截
+if(location.href.indexOf('login.html') === -1){
+  // console.log(1);
+  $.ajax({
+    type: 'get',
+    url: '/employee/checkRootLogin',
+    dataType: 'json',
+    success: function( info ) {
+      // console.log(info);
+      if(info.error === 400){
+        location.href = 'login.html';
+      }
+    }
+  })
+};
+//首页公共部分
+$(function() {
+
+  //侧边栏点击效果
+  $('.lt-aside .category').on('click',function(){
+    $('.content').slideToggle();
+  });
+  //头部点击侧边栏动画效果
+  $('.lt-topbar .meau').on('click', function(){
+    $('.lt-aside').toggleClass('current');
+    $('.lt-topbar').toggleClass('current');
+    $('.lt-main').toggleClass('current');
+  });
+  //点击退出登录效果
+  $('.lt-topbar .logout').on('click', function(){
+    $('.modal').modal({
+      show: true
+    });
+  })
+  //点击模态框退出
+  $('.btn-logout').on('click', function(){
+    // location.href = 'login.html';
+    $.ajax({
+      type: 'get',
+      url: '/employee/employeeLogout',
+      dataType: 'json',
+      success: function( info ){
+        // console.log(info);
+        if (info.success){
+          location.href = 'login.html';
+        }
+      }
+    })
+  });
+
+});
