a02f84535b7e297b500bbb7aba9efea49e78e12b
lyp <347989770@qq.com>
2018-08-19 20:13:56 +0800

    用户页面完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -2,6 +2,8 @@
 $(function(){
   var currentPage = 1;
   var pageSize = 5;
+  var currentId;
+  var isDelete;
 
   render();
   function render () {
@@ -14,13 +16,48 @@ $(function(){
       },
       dataType: 'json',
       success: function( info ) {
-        console.log(info);
+        // console.log(info);
         var htmlStr = template('userTpl', info);
         $('.lt-main tbody').html(htmlStr);
+        //分页
+        $('.paginator').bootstrapPaginator({
+          bootstrapMajorVersion: 3,
+          currentPage: info.page,
+          totalPages: Math.ceil(info.total / info.size),
+          onPageClicked: function( a, b, c, page) {
+            currentPage = page;
+            render();
+          }
+        })
       }
     })
   }
+  $('.lt-main tbody').on('click','.btn', function() {
+    // console.log(1);
+    currentId = $(this).parent().parent().data('id');
+    isDelete = $(this).hasClass('btn-danger') ? 0 : 1;
+    // console.log(currentId);
+    $('.isDeleteModal .modal').modal('show');
 
+  })
+  $('.btn-isDelete').on('click', function() {
+    $.ajax({
+      type: 'post',
+      url: '/user/updateUser',
+      data: {
+        id: currentId,
+        isDelete: isDelete
+      },
+      dataType: 'json',
+      success: function( info ) {
+        // console.log(info);
+        if ( info.success ) {
+          $('.isDeleteModal .modal').modal('hide');
+          render();
+        } 
+      }
+    })
+  })
 
 
 
