b8ea6b7a8d93f29de5727bca7247bc45e5e1266c
lyp <347989770@qq.com>
2018-08-20 00:01:22 +0800

    二级页面添加分类框架完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -32,9 +32,54 @@ $(function(){
     })
   }
 
+  //点击添加分类
+  $('.addCategory .btn').on('click',function() {
+    $('.addModal').modal('show');
+    $.ajax({
+      type: 'get',
+      url: '/category/queryTopCategoryPaging',
+      data: {
+        page: 1,
+        pageSize: 100
+      },
+      dataType: 'json',
+      success: function(info) {
+        console.log(info);
+        var htmlStr = template('addTpl', info);
+        $('#addSecond ul').html(htmlStr);
+      }
+    })
+  })
+
+  //点击选中一级分类
+  // $('#addSecond li').each(function(v,i){
+  //   $(this).on('click',function(){
+  //     console.log(1)
+  //   })
+  // })
+  $('#addSecond ul').on('click','a', function(){
+    // console.log(11);
+    // console.log($(this).html());
+    $('#addSecond .add-text').html($(this).html());
+
+  })
 
 
+  $('#addSecond').bootstrapValidator({
+    fields: {
 
+    }
+  })
 
+  $("#fileupload").fileupload({
+    dataType:"json",
+    //e：事件对象
+    //data：图片上传后的对象，通过data.result.picAddr可以获取上传后的图片地址
+    done:function (e, data) {
+      console.log(data);
+      var imgUrl = data.result.picAddr;
+      $('#addSecond img').attr('src', imgUrl);
+    }
+});
 
 })
