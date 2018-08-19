cadd03d3322823e5b4adbb1a3b5ef4b1585deca6
lyp <347989770@qq.com>
2018-08-19 21:13:04 +0800

    第一分类页功能完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -15,13 +15,62 @@ $(function() {
       },
       dataType: 'json',
       success: function( info ) {
-        console.log(info);
+        // console.log(info);
         var htmlStr = template('firstTpl', info);
         $('.lt-main tbody').html(htmlStr);
+        //分页
+        $('.paginator').bootstrapPaginator({
+          bootstrapMajorVersion: 3,
+          currentPage: info.page,
+          totalPages: Math.ceil(info.total / info.size),
+          onPageClicked: function(a, b, c, page) {
+            currentPage = page;
+            render();
+          }
+        })
       }
     })
   }
-
-
+  //点击添加分类
+  $('.addCategory .btn').on('click', function() {
+    // console.log(111);
+    $('.addModal').modal('show');
+  })
+ 
+  //表单校验
+  $('#addCategory').bootstrapValidator({
+    fields: {
+      categoryName: {
+        validators: {
+          notEmpty: {
+            message: '请输入一级分类名称'
+          }
+        }
+      }
+    },
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    }
+  })
+  $('#addCategory').on('success.form.bv', function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'post',
+      url: '/category/addTopCategory',
+      data: $('#addCategory').serialize(),
+      dataType: 'json',
+      success: function( info ) {
+        // console.log(info);
+        if( info.success ) {
+          $('.addModal').modal('hide');
+          currentPage = 1;
+          render();
+          $('#addCategory').data('bootstrapValidator').resetForm(true);
+        }
+      }
+    })
+  })
 
 })
